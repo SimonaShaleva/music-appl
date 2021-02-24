0c9bafd7bfcb3914f77ca2957ac6f18fbc552774
SimonaShaleva <SimonaShaleva@github.com>
2021-02-24 14:24:52 +0200

    name changes

--- src/test/java/com/example/music_appl/MusicAppApplicationTests.java
+++ src/test/java/com/example/music_appl/MusicAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringExamApplicationTests {
+class MusicAppApplicationTests {
 
     @Test
     void contextLoads() {
